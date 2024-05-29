a857e2d4695ed3c147ca23a9e9b42bad3c8b87f7
cherry <70464077+saicherry93479@users.noreply.github.com>
2024-05-30 01:34:35 +0530

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,5 @@
 import Comcast from "../assets/Comcast.png";
+import RESUME from "../assets/RESUME.pdf";
 
 const Experience = () => {
   return (
@@ -86,7 +87,8 @@ const Experience = () => {
           </ol>
           <a
             className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
-            href="#"
+            href={RESUME}
+            target="_blank"
           >
             Download Resume
             <svg
